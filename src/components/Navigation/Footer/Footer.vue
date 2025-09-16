f7c43997a92544cc9cd1096bd6e91693d5aa9919
austin-mel <austinmelendez77@gmail.com>
2025-09-16 13:45:56 -0700

    Update footer text mobile responsiveness

--- src/components/Navigation/Footer/Footer.vue
+++ src/components/Navigation/Footer/Footer.vue
@@ -8,7 +8,7 @@ const current_year = ref(new Date().getFullYear());
 <template>
   <footer class="w-full px-6 py-6 mt-auto">
         <div class="flex flex-col items-center justify-between mx-auto max-w-7xl sm:flex-row">
-            <div class="mb-4 text-sm sm:mb-0">
+            <div class="mb-4 ml-12 text-xs sm:text-sm sm:mb-0">
                 &copy; {{ current_year }} Austin Melendez. All Rights Reserved.
             </div>
             <div class="flex space-x-4">
