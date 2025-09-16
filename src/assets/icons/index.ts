45bf11dc95fadee48b1c2813a5285b5d970084b1
austin-mel <austinmelendez77@gmail.com>
2025-09-16 13:54:47 -0700

    Add icon svgs to asses

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,4 @@
-import crossIcon from "./cross.icon.vue";
+import PlusIcon from "./PlusIcon.vue";
 import LinkedInIcon from "./LinkedInIcon.vue";
 import LoaderIcon from "./LoaderIcon.vue";
 import LockIcon from "./LockIcon.vue";
@@ -6,7 +6,7 @@ import LogoutIcon from "./LogoutIcon.vue";
 import MailIcon from "./MailIcon.vue";
 import MoonIcon from "./MoonIcon.vue";
 import SunIcon from "./SunIcon.vue";
-import userIcon from "./user.icon.vue";
+import UserIcon from "./UserIcon.vue";
 import WebIcon from "./WebIcon.vue";
 import GithubIcon from "./GithubIcon.vue";
 import AdminIcon from "./AdminIcon.vue";
@@ -14,9 +14,17 @@ import DoctorIcon from "./DoctorIcon.vue";
 import JHAdminIcon from "./JHAdminIcon.vue";
 import PharmaIcon from "./PharmaIcon.vue";
 import EducIcon from "./EducIcon.vue";
+import EyeIcon from "./EyeIcon.vue";
+import ArrowLeftIcon from "./ArrowLeftIcon.vue";
+import ArrowRightIcon from "./ArrowRightIcon.vue";
+import CheckIcon from "./CheckIcon.vue";
+import CloseIcon from "./CloseIcon.vue";
+import EditIcon from "./EditIcon.vue";
+import RejectIcon from "./RejectIcon.vue";
+import SaveIcon from "./SaveIcon.vue";
 
 export {
-    crossIcon,
+    PlusIcon,
     LinkedInIcon,
     LoaderIcon,
     LockIcon,
@@ -24,7 +32,7 @@ export {
     MailIcon,
     MoonIcon,
     SunIcon,
-    userIcon,   
+    UserIcon,   
     WebIcon,
     GithubIcon,
     AdminIcon,
@@ -32,4 +40,13 @@ export {
     JHAdminIcon,
     PharmaIcon,
     EducIcon,
+    EyeIcon,
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    CheckIcon,
+    CloseIcon,
+    EditIcon,
+    RejectIcon,
+    SaveIcon,
+    
 }
